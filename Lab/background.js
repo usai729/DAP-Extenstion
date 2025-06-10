ff4d7c58947282b110d217eefa30cd80cf3aafe6
usa729 <usai7261@gmail.com>
2025-06-10 16:01:12 +0530

    `Update Lab background and content script to change default URL and add message passing functionality.`

--- Lab/background.js
+++ Lab/background.js
@@ -1,8 +1,16 @@
 chrome.action.onClicked.addListener(() => {
 	chrome.windows.create({
-		url: "https://www.google.com",
+		url: "https://www.example.com",
 		type: "popup",
 		width: 800,
 		height: 600,
 	});
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	console.log("Received message from content script:", request);
+
+	chrome.extension
+		.getBackgroundPage()
+		.postMessage({ type: "custom", data: request }, "*");
 });
--- Lab/contentScript.js
+++ Lab/contentScript.js
@@ -1,3 +1,19 @@
+let helpWidget_html = `
+
+  <div id="helpWidget"></div>
+`;
+
+let helpWidget = document.createElement("div");
+helpWidget.innerHTML = helpWidget_html;
+document.body.appendChild(helpWidget);
+
 window.addEventListener("mouseover", (event) => {
-	console.log(event);
+	chrome.runtime.sendMessage({
+		type: "elementHover",
+		selector: event,
+	});
+});
+
+window.addEventListener("message", (event) => {
+	console.log("Received from background script:", event);
 });
